8cd5ee9c825c4ca0ad73357d4994d35685e3fb38
jira <jirayuuu@hotmail.com>
2022-12-09 16:50:31 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -31,18 +34,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
-  void initState() {
+  void initState() {}
 
-  }
-
-
-  Barcode? result;
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   WebViewController? _webViewController;
-
 
   Widget buildWebView() {
     return WebView(
@@ -54,25 +51,30 @@ class _MyHomePageState extends State<MyHomePage> {
         _webViewController?.loadUrl(Uri.dataFromString(fileContent,
                 mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
             .toString());
+      },
+      javascriptChannels: <JavascriptChannel>{
+        JavascriptChannel(
+          name: 'messageHandler',
+          onMessageReceived: (JavascriptMessage message) async {
+            print("message from the web view=\"${message.message}\"");
 
-        javascriptChannels:
-        <JavascriptChannel>{
-          JavascriptChannel(
-            name: 'messageHandler',
-            onMessageReceived: (JavascriptMessage message) {
-              print("message from the web view=\"${message.message}\"");
-            },
-          )
-        };
+            // await Navigator.of(context).push(MaterialPageRoute(
+            //   builder: (context) => const QRViewExample(),
+            // ));
 
-        final script = "document.getElementById('value').innerText=\"testttt\"";
-        _webViewController?.evaluateJavascript(script);
-        super.initState();
+            var result = await Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                return QRViewExample();
+              }),
+            );
+            final script =
+                "document.getElementById('value').innerText=\"${result}\"";
+            _webViewController?.evaluateJavascript(script);
+          },
+        )
       },
     );
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -81,5 +83,87 @@ class _MyHomePageState extends State<MyHomePage> {
         child: buildWebView(),
       ),
     );
+  }
+}
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) async {
+      //result = scanData;
+      controller.pauseCamera();
+      Navigator.pop(context, scanData.code);
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
